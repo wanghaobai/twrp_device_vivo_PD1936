2ad9c293a8c6efa987b02d6d671e449b4f41b067
Nanhumly <1607616422@qq.com>
2022-09-24 10:02:59 +0000

    PD1936: initial commit

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_PD1936-user
+add_lunch_combo omni_PD1936-userdebug
+add_lunch_combo omni_PD1936-eng
